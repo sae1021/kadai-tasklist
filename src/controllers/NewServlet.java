30455d0547bf623e8473ccd75d025c0bb8e28cfa
sae.sonoda <ts1021pri@gmail.com>
2022-02-20 17:03:09 +0900

    ShowServlet.javaを作成

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -11,9 +11,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import models.Tasks;
 
-/**
- * Servlet implementation class NewServlet
- */
 @WebServlet("/new")
 public class NewServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
